675595e75499b8fdb2116eb03667aa25a7a97172
Paul-Mathias Logue <vcs@ethnarque.fr>
2024-06-29 11:07:21 +0200

    Add direnv module

--- machines/magda.nix
+++ machines/magda.nix
@@ -4,6 +4,7 @@
   appearance.fonts.enable = false;
 
   apps.emacs.enable = true;
+  apps.direnv.enable = true;
   apps.firefox.enable = true;
   apps.git.enable = true;
   apps.gpg.enable = true;
---
+++ modules/apps/direnv.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+let
+  inherit (lib) mkEnableOption mkIf mkMerge;
+  inherit (pkgs) stdenv;
+
+  cfg = config.apps.direnv;
+in
+{
+  options.apps.direnv = {
+    enable = mkEnableOption ''
+      direnv
+    '';
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    { 
+      programs.direnv.enable = true; 
+      programs.direnv.nix-direnv.enable = true; 
+    }
+
+    (mkIf config.programs.zsh.enable {
+       programs.direnv.enableZshIntegration = true;
+    })
+  ]);
+}
