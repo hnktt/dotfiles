2e2fa612944a3eb5f6bb0b34647110b8dcc4d842
Paul-Mathias Logue <vcs@ethnarque.fr>
2024-12-01 01:17:47 +0100

    Dump flakes

--- machines/magda.nix
+++ machines/magda.nix
@@ -3,13 +3,13 @@
 {
   appearance.fonts.enable = false;
 
-  apps.emacs.enable = true;
+  #apps.emacs.enable = true;
   apps.direnv.enable = true;
-  apps.firefox.enable = true;
+  #apps.firefox.enable = true;
   apps.git.enable = true;
-  apps.gpg.enable = true;
-  apps.pass.enable = true;
-  apps.neovim.enable = false;
+  #apps.gpg.enable = true;
+  #apps.pass.enable = true;
+  #apps.neovim.enable = false;
   apps.zsh.enable = true;
 
   home.stateVersion = "24.05";
